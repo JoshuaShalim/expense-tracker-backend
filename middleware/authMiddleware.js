d578e5fa0d22c48b0ebca03932ca859df92ac4aa
unknown <joshuashalim15@gmail.com>
2025-08-29 21:56:30 +0500

    backend Initial comment added user login user get info and profile photo upload with postman

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const protect = async (req, res, next) => {
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    try {
+      const token = req.headers.authorization.split(" ")[1];
+      console.log("Token received:", token);
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      console.log("Decoded token:", decoded);
+
+      const user = await User.findById(decoded.id).select("-password");
+      if (!user) {
+        return res.status(401).json({ message: "User not found" });
+      }
+
+      req.user = user;
+      next();
+    } catch (err) {
+      console.error("JWT error:", err.message);
+      return res.status(401).json({ message: "Not authorized, token failed" });
+    }
+  } else {
+    return res.status(401).json({ message: "Not authorized, no token" });
+  }
+};
+
+module.exports = { protect };
