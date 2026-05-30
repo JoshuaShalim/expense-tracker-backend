1f78682ab56059a899add78537a8f77438996197
Joshua Shalim <89259536+JoshuaShalim@users.noreply.github.com>
2026-05-30 17:05:31 +0300

    Clean up server.js by removing unnecessary code
    Removed unused routes and middleware from server.js.

--- server.js
+++ server.js
@@ -1,34 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.json({
-    route: "/"
-  });
-});
-
-app.get("/health", (req, res) => {
-  res.json({
-    route: "/health"
-  });
-});
-
-app.get("/test", (req, res) => {
-  res.json({
-    route: "/test"
-  });
-});
-
-// NO AUTH ROUTES
-app.get("/api/v1/auth/ping", (req, res) => {
-  res.json({
-    route: "/api/v1/auth/ping"
+    url: req.originalUrl,
+    path: req.path
   });
 });
 
