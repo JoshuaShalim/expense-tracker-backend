3e88183baf7db3381c41be396d6859021382d653
Joshua Shalim <89259536+JoshuaShalim@users.noreply.github.com>
2026-05-30 18:11:54 +0300

    Set up Express server with CORS and health check

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+const connectDB = require("./config/db");
 
 const app = express();
 
-app.get("*", (req, res) => {
-  res.status(200).send("WORKING");
+connectDB();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://expense-tracker-eta-ashy-39.vercel.app"
+    ],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok"
+  });
 });
+
+app.use("/api/v1/auth", authRoutes);
 
 module.exports = app;
