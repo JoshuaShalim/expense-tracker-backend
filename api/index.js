e46f400226f5833f67f7170e98866359d9f65d96
Joshua Shalim <89259536+JoshuaShalim@users.noreply.github.com>
2026-05-30 15:05:18 +0300

    Removed database connection middleware.
    Removed database connection middleware from the serverless app.

--- api/index.js
+++ api/index.js
@@ -1,15 +1,4 @@
 const serverless = require("serverless-http");
 const app = require("../server");
-const connectDB = require("../config/db");
-
-// Simple sync middleware - async middleware doesn't work well with serverless-http
-app.use((req, res, next) => {
-  // Try to connect but don't block - let the route handlers manage DB connection
-  connectDB().then(() => next()).catch(err => {
-    console.error("DB connection error:", err.message);
-    // Don't block the request - let it proceed and fail at DB level if needed
-    next();
-  });
-});
 
 module.exports = serverless(app);
