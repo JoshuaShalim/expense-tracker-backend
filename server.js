cf1e135415ca3c04633159d4d9ddaf4b164a38fe
Joshua Shalim <89259536+JoshuaShalim@users.noreply.github.com>
2026-05-30 16:41:53 +0300

    Add CORS support and update health route response

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+
 app.get("/health", (req, res) => {
   res.json({
-    status: "ok",
-    message: "health route working."
+    status: "ok"
   });
 });
 
