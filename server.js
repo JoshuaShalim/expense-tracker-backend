b8619b37b7401783457a8ea55a939582b60adbe8
Joshua Shalim <89259536+JoshuaShalim@users.noreply.github.com>
2026-05-30 17:36:16 +0300

    Refactor server routes and simplify responses

--- server.js
+++ server.js
@@ -1,24 +1,9 @@
-require("dotenv").config();
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({
-    route: "/"
-  });
-});
-
-app.get("/health", (req, res) => {
-  res.json({
-    route: "/health"
-  });
-});
-
-app.get("/ping", (req, res) => {
-  res.json({
-    route: "/ping"
-  });
+app.get("*", (req, res) => {
+  res.status(200).send("WORKING");
 });
 
 module.exports = app;
